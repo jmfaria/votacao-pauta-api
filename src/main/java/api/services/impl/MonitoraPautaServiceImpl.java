b05a3c83a3d7f2a3a66b98e5f3060c360bfbe58e
JMF <jmfaria1@hotmail.com>
2020-11-10 16:33:54 -0300

    Alterada a publicação de mensagem para JSON

--- src/main/java/api/services/impl/MonitoraPautaServiceImpl.java
+++ src/main/java/api/services/impl/MonitoraPautaServiceImpl.java
@@ -2,9 +2,14 @@ package api.services.impl;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import api.entities.Pauta;
 import api.services.MensageriaService;
@@ -13,6 +18,8 @@ import api.services.VotacaoService;
 
 @Service
 public class MonitoraPautaServiceImpl {
+	
+	private static final Logger log = LoggerFactory.getLogger(MonitoraPautaServiceImpl.class);
 
 	@Autowired
 	private PautaService pautaService;
@@ -22,7 +29,7 @@ public class MonitoraPautaServiceImpl {
 	private VotacaoService votacaoService;
 
 	@Scheduled(fixedDelay = 1000 * 60)
-	public void agendamentoParaFinaliarVotacaoDePauta() {
+	public void agendamentoParaFinaliarVotacaoDePauta(){
 		
 		this.pautaService.listarPautasNaoEncerradas().forEach(pauta -> {
 			
@@ -31,7 +38,17 @@ public class MonitoraPautaServiceImpl {
 				
 				pauta.setEncerrada(true);
 				this.pautaService.finalizarPauta(pauta);
-				this.mensageriaService.publicarMensagemNaFila(this.votacaoService.resultadoVotacao(pauta).toString());
+				try {
+					this.mensageriaService.publicarMensagemNaFila(
+							//this.votacaoService.resultadoVotacao(pauta).toString()
+							new ObjectMapper().writeValueAsString(
+									this.votacaoService.resultadoVotacao(pauta)
+									)
+							);
+				} catch (JsonProcessingException e) {					
+					log.error(e.getMessage());
+					e.printStackTrace();
+				}
 			}
 			
 		});
