9f960f0382c2f9bfd50073483f5506895e6d0a07
jmfaria <jmfaria1@hotmail.com>
2021-03-02 09:00:14 -0300

    Alterando de ActiveMQ para Kafka

--- src/main/java/api/config/JmsConfig.java
+++ src/main/java/api/config/JmsConfig.java
@@ -1,61 +1,45 @@
 package api.config;
 
-import javax.jms.ConnectionFactory;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
-@EnableJms
 public class JmsConfig {
 
-    @Value("${spring.activemq.broker-url}")
+	@Value(value = "${kafka.broker-url}")
     private String brokerUrl;
 
-    @Value("${spring.activemq.user}")
-    private String user;
-
-    @Value("${spring.activemq.password}")
-    private String password;
-
     @Bean
-    public ActiveMQConnectionFactory connectionFactory() {
+    public ProducerFactory<String, String> producerFactory() {
     	
-    	log.info("Configurando o suporte ao ActiveMQ");
-        if ( "".equals(user) ) {
-            return new ActiveMQConnectionFactory(brokerUrl);
-        }
-        return new ActiveMQConnectionFactory(user, password, brokerUrl);
+    	log.info("Configurando JMS Kafka");
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(
+          ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, 
+          brokerUrl);
+        configProps.put(
+          ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, 
+          StringSerializer.class);
+        configProps.put(
+          ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, 
+          StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public JmsListenerContainerFactory<?> jmsFactoryTopic(ConnectionFactory connectionFactory,
-                                                  DefaultJmsListenerContainerFactoryConfigurer configurer) {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        configurer.configure(factory, connectionFactory);
-        factory.setPubSubDomain(true);
-        return factory;
-    }
-
-    @Bean
-    public JmsTemplate jmsTemplate() {
-        return new JmsTemplate(connectionFactory());
-    }
-
-    @Bean
-    public JmsTemplate jmsTemplateTopic() {
-        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory());
-        jmsTemplate.setPubSubDomain( true );
-        return jmsTemplate;
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
